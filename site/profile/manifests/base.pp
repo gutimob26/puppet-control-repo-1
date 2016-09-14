f2f204fdf8807c07ec43598c2a8d0afb8d5604a1
Laura <lm@puppet.com>
2016-09-14 17:59:42 +0000

    Add a profile base

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,2 +1,28 @@
+# This is a profile base that does stuff
 class profile::base {
+  user { 'root':
+    ensure   => 'present',
+    comment  => 'root',
+    gid      => '0',
+    home     => '/root',
+    password => '$1$Tb9hAyyD$iWQhIMeYbBg7ES6vINn6H.',
+    shell    => '/bin/bash',
+    uid      => '0',
+  }
+
+  user { 'laura':
+    ensure     => present,
+    managehome => true,
+  }
+
+  class { '::ntp':
+    servers => ['0.pool.ntp.org','1.pool.ntp.org'],
+  }
+
+  include ::epel
+
+  package { 'vim-enhanced':
+    ensure => installed,
+  }
+
 }
